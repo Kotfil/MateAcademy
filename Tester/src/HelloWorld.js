aa3d90c90b7fc5c21e0eb18cfcfa132d48e457ea
FIlipp <filippkotenko@outlook.com>
2020-05-09 16:12:41 +0300

    Counter incr++

--- Tester/src/HelloWorld.js
+++ Tester/src/HelloWorld.js
@@ -2,12 +2,19 @@ import React, {Component} from "react";
 import "./HelloWorld.css";
 
  class HelloWorld extends Component {
+
+
     render() {
+
         return (
             <div className="helloContainer">
                 <h1>
                     Привет,мир!
                 </h1>
+                <div className="1">
+
+
+                </div>
             </div>
         );
     }
